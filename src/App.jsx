058120bb104a0d8f4ae6feecc9999fbc62203778
Nydia García <nina@yahoo.com.mx>
2025-07-19 17:24:02 -0600

    Actualización de texto en App.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import styles from './App.module.css';
 function App() {
 return (
 <div className={styles.container}>
+<h1>Hola desde Netlify con despliegue continuo</h1>
 <h1 className={styles.title}>Lista de Productos</h1>
+
 <ProductList />
 </div>
 );
